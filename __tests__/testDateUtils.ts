01aa9ee6ab9be0019f24c792506e28d60dde329c
Muhammad Irham Rasyidi Bin Zainal <h0910104@gmail.com>
2023-05-07 19:36:48 +0800

    Add redundancy to parseLocalDate

--- __tests__/testDateUtils.ts
+++ __tests__/testDateUtils.ts
@@ -13,4 +13,35 @@ describe("Date Utils", () => {
 
     expect(actualTimestamp).toBe(expectedTimestamp);
   });
+
+  it.each([
+    {
+      localDateString: "2023-05-06 18:01",
+      expectedTimestamp: Date.UTC(2023, 4, 6, 18, 1) - 8 * 60 * 60 * 1000,
+    },
+    {
+      localDateString: "2023-05-06 18",
+      expectedTimestamp: Date.UTC(2023, 4, 6, 18, 0) - 8 * 60 * 60 * 1000,
+    },
+    {
+      localDateString: "2023-05-06",
+      expectedTimestamp: Date.UTC(2023, 4, 6, 0, 0) - 8 * 60 * 60 * 1000,
+    },
+    {
+      localDateString: "2023-05",
+      expectedTimestamp: Date.UTC(2023, 4, 1, 0, 0) - 8 * 60 * 60 * 1000,
+    },
+    {
+      localDateString: "2023",
+      expectedTimestamp: Date.UTC(2023, 0, 1, 0, 0) - 8 * 60 * 60 * 1000,
+    },
+  ])(
+    'parseLocalDate with less than 5 fields: "$localDateString"',
+    ({ localDateString, expectedTimestamp }) => {
+      const actualDate = parseLocalDate(localDateString);
+      const actualTimestamp = actualDate.getTime();
+
+      expect(actualTimestamp).toBe(expectedTimestamp);
+    }
+  );
 });
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,7 +1,10 @@
 /**
  * Parses a local date/time string that has no specified timezone.
  *
- * @param dateTimeString A date and time string in the format "YYYY-MM-DD HH:mm"
+ * @param dateTimeString A date and time string in the format "YYYY-MM-DD HH:mm".
+ * Any fields smaller than 'minutes' will be ignored. If there are less than 5 numeric
+ * fields, they will be set to some default values. At least one field (i.e. the year
+ * field) must be present.
  * @param timezoneOffsetInMilliseconds The offset between the local timezone and UTC.
  * If the timezone has a positive offset (e.g. UTC+2), this value should be positive.
  * @returns A Date object representing the local time.
@@ -17,10 +20,10 @@ export const parseLocalDate = (
   const tokens = dateTimeString.split(/\D/).map((token) => parseInt(token, 10));
   const pseudoTimestamp = Date.UTC(
     tokens[0],
-    tokens[1] - 1,
-    tokens[2],
-    tokens[3],
-    tokens[4]
+    (tokens[1] ?? 1) - 1,
+    tokens[2] ?? 1,
+    tokens[3] ?? 0,
+    tokens[4] ?? 0
   );
 
   return new Date(pseudoTimestamp - timezoneOffsetInMilliseconds);
