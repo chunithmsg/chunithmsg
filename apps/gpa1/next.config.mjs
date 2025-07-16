a55891089c0c915e2d488f5d704996638f4a6ce3
Blerargh <terrysi237@gmail.com>
2025-07-16 17:38:55 +0800

    debug pt2

---
+++ apps/gpa1/next.config.mjs
@@ -0,0 +1,38 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import remarkGFM from 'remark-gfm';
+import remarkUnwrapImage from 'remark-unwrap-images';
+import createMDX from '@next/mdx';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true' || false,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+  // webpack: (config) => ({
+  //   ...config,
+  //   module: {
+  //     ...config.module,
+  //     rules: config.module.rules.concat([
+  //       {
+  //         test: /\.md/,
+  //         type: 'asset/source',
+  //       },
+  //     ]),
+  //   },
+  // }),
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGFM, remarkUnwrapImage],
+    rehypePlugins: [],
+  },
+});
+
+export default bundleAnalyzer(withMDX(nextConfig));
---
+++ apps/gpa1/src/mdx-components.tsx
@@ -0,0 +1,28 @@
+import type { MDXComponents } from 'mdx/types';
+
+/**
+ * Let's not mess with the MDX components
+ *
+ * @param components
+ * @returns
+ */
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    a: ({ href, children }) => (
+      <a href={href} className="underline">
+        {children}
+      </a>
+    ),
+    table: ({ children }) => (
+      <div className="my-6 w-full overflow-y-auto">
+        <table>{children}</table>
+      </div>
+    ),
+    // TODO: fix the img issue where it spawns fcp warnings
+    img: (props) => (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img {...props} alt="" className="border-2 border-primary" />
+    ),
+    ...components,
+  };
+}
